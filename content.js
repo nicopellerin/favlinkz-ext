fc79601a8c15c7185c75428e1730d56c62b19f86
Nico <nickypellerin@gmail.com>
2020-05-21 16:52:38 -0400

    Remove event listener in content.js

--- content.js
+++ content.js
@@ -1,6 +1,1 @@
 /*global chrome*/
-chrome.runtime.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { type: "getWindow" });
-  });
-});
